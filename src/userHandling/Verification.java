cf4d7e9818be2679ae7b8f1126270d1fed2a8a9e
KennyAden <adenkenny@gmail.com>
2016-09-21 21:00:26 +1200

    Logging improvements

--- src/userHandling/Verification.java
+++ src/userHandling/Verification.java
@@ -1,5 +1,8 @@
 package userHandling;
 
+import util.Logging;
+import util.Logging.Levels;
+
 /**
  * A class dealing with a verifying a user's login details.
  *
@@ -18,16 +21,16 @@ public final class Verification {
 	 * user exists. If the user exists the entered password is then hashed and compared
 	 * against the hash stored in the database for the user matching the username that
 	 * the login attempt entered.
-	 * 
+	 *
 	 * If the user account does not exist or the hash generated from the inputed
 	 * password does not match the hash stored in the database this method will
 	 * return null rather than the logged in user.
-	 * 
+	 *
 	 * @param username The username of the account the user wants to access.
 	 * @param password The password of the account the user wants to access.
 	 * @return The user account which the user logged into.
 	 */
-	
+
 	public static User login(String username, String password) {
 
 		if(Register.userExists(username)) { //Check to see if user exits.
@@ -35,11 +38,16 @@ public final class Verification {
 
 			if(Hashing.verifyPassword(password, user.getHash())) { //Correct password.
 				password = null;
+
+				Logging.logEvent(Verification.class.getName(), Levels.EVENT, username + " logged in successfully.");
+
 				return user;
 			}
 
 			password = null;
 		}
+
+		Logging.logEvent(Verification.class.getName(), Levels.EVENT, "An unsuccessful attempt to login from " + username);
 
 		return null; //The login failed, either wrong password or no user with the name exists.
 	}
--- src/util/Logging.java
+++ src/util/Logging.java
@@ -6,34 +6,107 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.Calendar;
 
+import util.Logging.Levels;
+
+/**
+ * A class to log events in a .log file.
+ *
+ * @author Aden
+ */
+
 public final class Logging {
+
+	/**
+	 * Enums representing the levels of events being sent to the server.
+	 *
+	 * WARNING should represent an error that we can recover from.
+	 * SEVERE should represent an error that we cannot recover from.
+	 * EVENT should represent any other event such as user joining.
+	 * OTHER should represent miscellaneous events.
+	 */
 
 	public enum Levels {
 		WARNING,
 		SEVERE,
-		EVENT;
+		EVENT,
+		OTHER; //Is this needed?
 	}
 
-	private static final File LOG_FILE = new File("logs/logs.log");
+	private Logging() {
+		throw new AssertionError(); //Shouldn't be intitialised.
+	}
+
+	private static final File LOG_FILE = new File("logs/logs.log"); //The file we will log to.
+
+	/**
+	 * Logs an event to the log file, this event will contain the time it was logged at,
+	 * the severity of the event, the class that the event was logged in, and a message
+	 * that can be specified.
+	 *
+	 * @param className A string representing the name of the class that even was logged in.
+	 * @param level An enum representing the severity of an event.
+	 * @param message A string representing the message that will be logged.
+	 */
 
 	public static void logEvent(String className, Levels level, String message) {
 		try (FileWriter fileWriter = new FileWriter(LOG_FILE, true);
 				BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-				PrintWriter printWriter = new PrintWriter(bufferedWriter)) {
+				PrintWriter printWriter = new PrintWriter(bufferedWriter)) { //Writer setup.
 
-				String timeStamp = new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
-
-				String logMessage = timeStamp + " - " + level.toString() + " in " + className + ": " + message;
-
-				printWriter.println(logMessage);
+				printWriter.println(new LogEvent(level, className, message).out()); //Print the message to log.
 		}
 
 		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
+
+	/**
+	 * A class representing an event that will be logged to the output file.
+	 *
+	 * @author Aden
+	 */
+
+	class LogEvent{
+
+		String level; //The level of the event.
+		String className; //The class the event was registered in.
+		String message; //The message to be logged.
+		String timeStamp; //The time that the event was logged.
+
+		/**
+		 * Creates a LogEvent that can be outputed to the logging file. This LogEvent will
+		 * contain the time that the event was logged, the severity of the event, the name
+		 * of the class that the event was logged in, and a message that is specified.
+		 *
+		 * @param levelEnum The severity of the event.
+		 * @param className A String representing the name of the origin class.
+		 * @param message A string of the message to be logged.
+		 */
+
+		LogEvent(Levels levelEnum, String className, String message) {
+
+			this.level = levelEnum.toString();
+			this.className = className;
+			this.message = message;
+			this.timeStamp = new SimpleDateFormat("HH:mm:ss") .format(Calendar.getInstance().getTime());
+
+		}
+
+		/**
+		 * Concatenates all the information and returns a full string that can then be logged.
+		 *
+		 * @return A string representing a LogEvent with all relevant information.
+		 */
+
+		String out() {
+			String toLog = this.timeStamp + " - " + this.level + " in " + this.className + ": " + this.message;
+
+			return toLog;
+		}
+	}
+
+
