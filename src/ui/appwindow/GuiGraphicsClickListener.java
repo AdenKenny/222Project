5e4de397cffd6078fe069b1525b668a6513976c0
~Clinton <normanclin@ecs.vuw.ac.nz>
2016-10-06 09:15:26 +1300

    added slave to send chat

--- src/ui/appwindow/GuiGraphicsClickListener.java
+++ src/ui/appwindow/GuiGraphicsClickListener.java
@@ -6,17 +6,21 @@ import gameWorld.Entity;
 public class GuiGraphicsClickListener implements GraphicsClickListener {
 
 	private MainWindow client;
-	
+
 	public GuiGraphicsClickListener(MainWindow client) {
 		this.client = client;
 	}
-	
+
 	@Override
 	public void onClick(Entity entity, boolean alt, int x, int y) {
 		if(entity==null) return;
-		
+
 		if(alt){
+			// display, then get the index of the selected option
 			client.displayItemOptions(entity.actions(), x, y);
+			// check Action.isClientAction()
+			//		if true -> Action.perform(client) !needs to be MainWindow
+			//		else send to server
 		}
 		else{
 			//get default left click option for the entity
