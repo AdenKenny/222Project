6c469be794d726cf052b34c478424ba7833b1ac6
Louis Thie <louisthie@orcon.net.nz>
2016-10-06 09:15:10 +1300

    added modelID field

--- src/gameWorld/objects/StationaryObject.java
+++ src/gameWorld/objects/StationaryObject.java
@@ -17,48 +17,50 @@ public class StationaryObject extends Entity {
 		DOOR,
 		// TODO: DROP
 	}
-	
+
 	private Type type;
 	private Set<Integer> items;
-	
+	private int modelID;
+
 	private boolean isOpen;
 	private long openTime;
 
 	public StationaryObject(ObjectModel model, Room room, int xPos, int yPos, Direction facing) {
 		super(room, xPos, yPos, model.getName(), model.getDescription(), facing);
-		
+
 		this.type = model.getType();
 		this.items = model.getItems();
-		
+		this.modelID = model.getID();
+
 		this.isOpen = false;
-		
+
 		if (this.type.equals(Type.CHEST)) {
 			this.actions.add(new Action() {
 				@Override
 				public String name() {
 					return "Open";
 				}
-	
+
 				@Override
 				public void perform(Object caller) {
 					if (!(caller instanceof Character)) {
 						return;
 					}
-					
+
 					Character ch = (Character) caller;
 					List<Integer> chItems = ch.getItems();
 					chItems.add(getRandomItem());
 				}
-	
+
 				@Override
 				public boolean isClientAction() {
 					return false;
 				}
-				
+
 			});
 		}
 	}
-	
+
 	public Integer getRandomItem() {
 		if (this.type.equals(Type.CHEST)) {
 			Integer[] ints = this.items.toArray(new Integer[this.items.size()]);
@@ -66,11 +68,11 @@ public class StationaryObject extends Entity {
 		}
 		return null;
 	}
-	
+
 	public Type getType() {
 		return this.type;
 	}
-	
+
 	public Set<Integer> getItems() {
 		return this.items;
 	}
