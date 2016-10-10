dcb9e345a373d1b2ee6b761c48f4a7458801be35
Louis Thie <louisthie@orcon.net.nz>
2016-10-10 13:51:31 +1300

    Fix null errors that resulted from previous change to Rooms/doors

--- src/gameWorld/rooms/Room.java
+++ src/gameWorld/rooms/Room.java
@@ -30,6 +30,7 @@ public class Room {
 		this.ID = Entity.getNewID();
 		this.floor = floor;
 		this.neighbours = new HashMap<>();
+		this.doors = new HashMap<>();
 		this.xPos = xPos;
 		this.yPos = yPos;
 		this.width = width;
@@ -41,6 +42,7 @@ public class Room {
 		this.floor = floor;
 		this.ID = Entity.getNewID();
 		this.neighbours = new HashMap<>();
+		this.doors = new HashMap<>();
 		this.xPos = builder.getxPos();
 		this.yPos = builder.getyPos();
 		this.width = builder.getWidth();
