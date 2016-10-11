e2dba06edd9940b6d117c44db2e2dcf83d41e37c
~Clinton <normanclin@ecs.vuw.ac.nz>
2016-10-11 15:17:00 +1300

    fixed null pointer character item attack

--- src/gameWorld/characters/Character.java
+++ src/gameWorld/characters/Character.java
@@ -348,6 +348,7 @@ public class Character extends Entity implements Buildable, Sendable, Cloneable 
 		int attack = attacker.getAttack(); // max ~1000
 		int defense = 0; // max 350
 		for (Item item : this.equips) {
+			if(item != null){
 			switch (item.getType()) {
 			case ARMOR:
 			case SHIELD:
@@ -357,6 +358,7 @@ public class Character extends Entity implements Buildable, Sendable, Cloneable 
 			// $CASES-OMITTED$
 			default:
 				break;
+			}
 			}
 		}
 
